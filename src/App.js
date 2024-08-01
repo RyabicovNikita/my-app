2bc92b84f18b850be3ae0f3358f57da8fea2416b
Рябиков Никита <134300789+RyabicovNikita@users.noreply.github.com>
2024-08-01 18:44:06 +0300

    task 1 realize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
+import { DateTime } from 'luxon';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [values, addNewValue] = useState([]);
+	const [userValue, setUserValue] = useState(null);
+	const [isErrorMsg, hideErrorMsg] = useState(false);
+
+	function onInputButtonClick() {
+		const userValue = prompt('Введите значение');
+		if (userValue.length < 3) {
+			hideErrorMsg(true);
+			return;
+		} else hideErrorMsg(false);
+		const createDateTime = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
+		setUserValue({ id: Date.now(), createDate: createDateTime, text: userValue });
+	}
+	function onAddButtonClick() {
+		addNewValue([...values, userValue]);
+		console.log(values);
+		setUserValue(null);
+	}
+
+	const emptyListMessage = <p className={styles['no-margin-text']}>Нет добавленных элементов</p>;
+
+	const valuesList = (
+		<ul className={styles.list}>
+			{values.map((value) => (
+				<li className={styles['list-item']} key={value.id}>
+					{value.text}
+				</li>
+			))}
+		</ul>
+	);
+
+	const errorMessage = <div className={styles.error}>Введенное значение должно содержать минимум 3 символа</div>;
+	return (
+		<div className={styles.app}>
+			<h1 className={styles['page-heading']}>Ввод значения</h1>
+			<p className={styles['no-margin-text']}>
+				Текущее значение <code>value</code>: "
+				<output className={styles['current-value']}>{userValue?.text}</output>"
+			</p>
+			{isErrorMsg && errorMessage}
+			<div className={styles['buttons-container']}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button className={styles.button} onClick={onAddButtonClick} disabled={!userValue?.text?.length}>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles['list-container']}>
+				<h2 className={styles['list-heading']}>Список:</h2>
+				{values.length === 0 ? emptyListMessage : valuesList}
+			</div>
+		</div>
+	);
 }
 
 export default App;
